a5bb72803b1f97d27ba3d55b2afa16e83a739aa2
Rahul Chaudhuri <mailrahulsre@gmail.com>
2021-02-07 05:31:09 +0000

    5th commit

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -2,5 +2,5 @@ username=$(cat password|grep user|awk -F"=" '{print $2}')
 
 password=$(cat password|grep password|awk -F"=" '{print $2}')
 dbName=flipkart
-echo "Below command will run.."
+echo "Below command will run and take mysql backup.."
 echo "mysqldump -u$username -p$password -hflipkart.aws.rds.com $dbName > $dbName.sql"
